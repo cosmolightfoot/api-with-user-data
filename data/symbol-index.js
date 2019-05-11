517206d28c65f36aecf465dc90bba2037c99ac1a
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-10 18:44:53 -0700

    pre refactor commit

---
+++ data/symbol-index.js
@@ -0,0 +1,50 @@
+const symbolIndex = [
+    {
+        code: '{W}',
+        src: '../../assets/white-mana.svg'
+    },
+    {
+        code: '{U}',
+        src: '../../assets/blue-mana.svg'
+    },
+    {
+        code: '{R}',
+        src: '../../assets/red-mana.svg'
+    },
+    {
+        code: '{G}',
+        src: '../../assets/green-mana.svg'
+    },
+    {
+        code: '{G}',
+        src: '../../assets/black-mana.svg'
+    },
+    {
+        code: '{S}',
+        src: '../../assets/snow-mana.svg'
+    },
+    {
+        code: '{C}',
+        src: '../../assets/diamond-mana.svg'
+    },
+    {
+        code: '{1}',
+        src: '../../assets/one-mana.svg'
+    },
+    {
+        code: '{2}',
+        src: '../../assets/two-mana.svg'
+    },
+    {
+        code: '{3}',
+        src: '../../assets/three-mana.svg'
+    },
+    {
+        code: '{4}',
+        src: '../../assets/four-mana.svg'
+    },
+    {
+        code: '{5}',
+        src: '../../assets/five-mana.svg'
+    },
+];
